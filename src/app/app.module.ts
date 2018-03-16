d923d5281b178a8381c0703a7cf2d4a821d44037
uuit <1812890566@qq.com>
2018-03-16 17:41:25 +0800

    ts-web init

---
+++ src/app/app.module.ts
@@ -0,0 +1,67 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { IssueComponent } from './issue/issue.component';
+import { RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectComponent } from './project/project.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { Err404Component } from './error/err404/err404.component';
+import { Err500Component } from './error/err500/err500.component';
+import { TabNavComponent } from './common/tab-nav/tab-nav.component';
+import { TableListComponent } from './templates/table-list/table-list.component';
+import { PaginationComponent } from './templates/pagination/pagination.component';
+import { ProfileComponent } from './personal/profile/profile.component';
+import { CreateComponent } from './issue/create/create.component';
+import {TeamComponent} from './team/team.component';
+
+
+// 路由配置
+const routeConfig: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'settings', loadChildren: './settings/routes/routes.module#RoutesModule'},
+  {path: 'team/:teamId', component: TeamComponent},
+  {path: 'project/:projectId', component: ProjectComponent},
+  {path: 'issue/:issueId', component: IssueComponent},
+  {path: 'profile/:profileId', component: ProfileComponent},
+  {path: 'err404', component: Err404Component},
+  {path: 'err500', component: Err500Component},
+  {path: '**', pathMatch: 'full', redirectTo: 'err404'}
+];
+
+// 路由配置
+// const teamRouteConfig: Routes = [
+//   {path: 'team/:teamId', component: TeamComponent}
+// ];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TeamComponent,
+    ProductComponent,
+    IssueComponent,
+    HomeComponent,
+    ProjectComponent,
+    HeaderComponent,
+    FooterComponent,
+    Err404Component,
+    Err500Component,
+    TabNavComponent,
+    TableListComponent,
+    PaginationComponent,
+    ProfileComponent,
+    CreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routeConfig)
+    /*RouterModule.forChild(teamRouteConfig)*/
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/team/team.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-team',
+  templateUrl: './team.component.pug',
+  styleUrls: ['./team.component.scss']
+})
+export class TeamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
